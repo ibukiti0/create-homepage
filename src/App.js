c6d68ecc054dff7cfcf316e3a6d480ee3b740599
ibukiti <ibukiti0115@gmail.com>
2021-11-27 05:09:00 +0900

    create-homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Title } from "./components/Title/Title";
 
 import classes from "./App.module.scss";
 import {Myintro} from "./components/my_intro/myintro";
+import { Pekocount } from "./components/peko_count/peko_count";
 
 function App() {
   return (
     <div className={classes.app_backgaround}>
       <Title/>
       <Myintro/>
+      <Pekocount/>
     </div>
   );
 }
