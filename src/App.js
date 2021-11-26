3532b6df112d21a5fb1c092564a13e4eb53a1f48
ibukiti <ibukiti0115@gmail.com>
2021-11-27 02:29:52 +0900

    create-homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Title } from "./components/Title/Title";
+import classes from "./App.module.scss";
 
 function App() {
   return (
-    <div>
+    <div className={classes.app_backgaround}>
       <Title/>
     </div>
   );
